75a5171294e2984a28c74c19d703186dd2632804
Elijah Manor <elijah.manor@gmail.com>
2022-09-04 13:53:55 -0500

    Cleanup general eslint warnings

--- contentScript.js
+++ contentScript.js
@@ -1,59 +1,77 @@
-$(function() {
-	chrome.extension.onRequest.addListener((request, sender, sendResponse) => {	
-		console.log("contentScript");
-		if (request.action == "getUserName")
-		{
-			let searchTerm = "", twitterUrl = "", userName = "", userHash = {}, userNames = [];
-			
-			$(document).find("a[href*='://twitter.com/'], a[href*='://www.twitter.com/'], a[href*='://twittercounter.com/']").each(function() {
-				const twitterUrl = $(this).attr("href"),
-				lastIndexOf = twitterUrl.lastIndexOf("/");
+$(function () {
+  chrome.extension.onRequest.addListener((request, sender, sendResponse) => {
+    console.log('contentScript')
+    if (request.action == 'getUserName') {
+      let userName = '',
+        userHash = {},
+        userNames = []
 
-				if (twitterUrl && twitterUrl.length > 0) {
-					userName = twitterUrl.substr(lastIndexOf + 1);
-					if (userHash[userName]) {
-						userHash[userName].count += 1;
-					} else {
-						userHash[userName] = { name: userName, count: 1 };
-					}
-				}
-			});
-			
-			const hashFrequency = [];
-			hashFrequency['jquery'] = $(':contains("jQuery"), :contains("JQuery"), :contains("JQuery"), :contains("Jquery"), :contains("JQUERY")').length;
-			hashFrequency['aspnetmvc'] = $(':contains("ASP.NET MVC"), :contains("ASP.Net MVC"), :contains("MVC")').length;
-			hashFrequency['aspnet'] = $(':contains("ASP.NET 4.0"), :contains("ASP.Net 4.0"), :contains("WebForms"), :contains("Web Forms"), :contains("runat=\"server\""), :contains("<asp:")').length;
-			hashFrequency['javascript'] = $(':contains("JavaScript"), :contains("Javascript"), :contains("javascript")').length;
-			hashFrequency['webdev'] = $(':contains("HTML"), :contains("HTML5"), :contains("CSS"), :contains("CSS3")').length;
-			hashFrequency['visualstudio'] = $(':contains("Visual Studio"), :contains("VS")').length;
-			const sortedHashFrequency = sortByNumericValue(hashFrequency);
+      $(document)
+        .find(
+          "a[href*='://twitter.com/'], a[href*='://www.twitter.com/'], a[href*='://twittercounter.com/']"
+        )
+        .each(function () {
+          const twitterUrl = $(this).attr('href'),
+            lastIndexOf = twitterUrl.lastIndexOf('/')
 
-			const mostFrequentHash = (a => {
-			   let top = 'misc'; 
-			   for (var i in a) { 
-					if (a[i].c > 0) {
-						top = a[i].v; 
-						break; 
-					}				   
-			   } 
-			   return top; 
-			})(sortedHashFrequency);
-			
-			for (var name in userHash) {
-				userNames.push( userHash[ name ] );
-			}			
-			sendResponse({userNames: userNames, hashTag: mostFrequentHash});	  
-		} else {
-			sendResponse({}); 
-		}
-	});
-});
+          if (twitterUrl && twitterUrl.length > 0) {
+            userName = twitterUrl.substr(lastIndexOf + 1)
+            if (userHash[userName]) {
+              userHash[userName].count += 1
+            } else {
+              userHash[userName] = { name: userName, count: 1 }
+            }
+          }
+        })
+
+      const hashFrequency = []
+      hashFrequency['jquery'] = $(
+        ':contains("jQuery"), :contains("JQuery"), :contains("JQuery"), :contains("Jquery"), :contains("JQUERY")'
+      ).length
+      hashFrequency['aspnetmvc'] = $(
+        ':contains("ASP.NET MVC"), :contains("ASP.Net MVC"), :contains("MVC")'
+      ).length
+      hashFrequency['aspnet'] = $(
+        ':contains("ASP.NET 4.0"), :contains("ASP.Net 4.0"), :contains("WebForms"), :contains("Web Forms"), :contains("runat="server""), :contains("<asp:")'
+      ).length
+      hashFrequency['javascript'] = $(
+        ':contains("JavaScript"), :contains("Javascript"), :contains("javascript")'
+      ).length
+      hashFrequency['webdev'] = $(
+        ':contains("HTML"), :contains("HTML5"), :contains("CSS"), :contains("CSS3")'
+      ).length
+      hashFrequency['visualstudio'] = $(
+        ':contains("Visual Studio"), :contains("VS")'
+      ).length
+      const sortedHashFrequency = sortByNumericValue(hashFrequency)
+
+      const mostFrequentHash = (a => {
+        let top = 'misc'
+        for (var i in a) {
+          if (a[i].c > 0) {
+            top = a[i].v
+            break
+          }
+        }
+        return top
+      })(sortedHashFrequency)
+
+      for (var name in userHash) {
+        userNames.push(userHash[name])
+      }
+      sendResponse({ userNames: userNames, hashTag: mostFrequentHash })
+    } else {
+      sendResponse({})
+    }
+  })
+})
 
 function sortByNumericValue(A) {
-	var B = [];
-	for (i in A) {
-		B.push({v:i,c:A[i]})
-    }    
-    return B.sort(function(x,y) { return y.c-x.c });
+  var B = []
+  for (let i in A) {
+    B.push({ v: i, c: A[i] })
+  }
+  return B.sort(function (x, y) {
+    return y.c - x.c
+  })
 }
-
--- popup.js
+++ popup.js
@@ -1,54 +1,59 @@
 $(() => {
-	$('#techTweet').bind('keyup', () => {
-		$('#charactersLeft').text(140 - $(this).val().length);
-	});		
-	
-	chrome.tabs.getSelected(null, tab => { 								
-		chrome.tabs.sendRequest(tab.id, {action: "getUserName"}, response => {
-			console.log("popup.js", { response });
-			let title = tab.title;
-			const byAuthor = response.userNames.length ? ' by @' + response.userNames[0].name + ' ' : ' ';
-			if (response.userNames.length) {
-				response.userNames = response.userNames.slice( 1 );
-			}
-			const hashTags = (response.hashTag ? '#' + response.hashTag + ' ' : '#misc');
+  $('#techTweet').bind('keyup', () => {
+    $('#charactersLeft').text(140 - $(this).val().length)
+  })
 
-			let cc = "";
-			$.each(response.userNames, (_index, user) => {
-				cc += "@" + user.name + " ";
-			});
-			if (cc) {
-				cc = " +" + cc;
-			}
+  chrome.tabs.getSelected(null, tab => {
+    chrome.tabs.sendRequest(tab.id, { action: 'getUserName' }, response => {
+      console.log('popup.js', { response })
+      let title = tab.title
+      const byAuthor = response.userNames.length
+        ? ' by @' + response.userNames[0].name + ' '
+        : ' '
+      if (response.userNames.length) {
+        response.userNames = response.userNames.slice(1)
+      }
+      const hashTags = response.hashTag ? '#' + response.hashTag + ' ' : '#misc'
 
-			const indexOfDash = title.indexOf("-");
-			const indexOfPipe = title.indexOf("|");
-			const indexOfArrow = title.indexOf("\u00BB");
-			if (indexOfDash > -1 || indexOfPipe > -1 || indexOfArrow > -1) {
-				title = title.substr(0, indexOfDash) || title.substr(0, indexOfPipe) || title.substr(0, indexOfArrow);
-				title = $.trim(title);
-			}
-			$('#techTweet').val(title + byAuthor + hashTags + cc);
+      let cc = ''
+      $.each(response.userNames, (_index, user) => {
+        cc += '@' + user.name + ' '
+      })
+      if (cc) {
+        cc = ' +' + cc
+      }
 
-			const tabUrl = tab.url;
-			const utmIndex = tabUrl.indexOf('?utm_source=feedburner');
-			if (utmIndex > -1) {
-				tabUrl = tabUrl.substr(0, utmIndex);
-			}
-			getAndAppendShortUrl(tabUrl, shortUrl => {
-				const techTweet = $('#techTweet');
-				const oldValue = techTweet.val();
-				techTweet.val(oldValue + " " + shortUrl + cc);
-				techTweet[0].focus();
-				techTweet[0].select();
-				techTweet.trigger('keyup');
-			});									
-		});
-	});		
-	
-	function getAndAppendShortUrl(longUrl, callback) {
-		chrome.extension.sendRequest({longUrl: longUrl}, response => {
-			callback( response.shortUrl );
-		});				
-	}	
-});
+      const indexOfDash = title.indexOf('-')
+      const indexOfPipe = title.indexOf('|')
+      const indexOfArrow = title.indexOf('\u00BB')
+      if (indexOfDash > -1 || indexOfPipe > -1 || indexOfArrow > -1) {
+        title =
+          title.substr(0, indexOfDash) ||
+          title.substr(0, indexOfPipe) ||
+          title.substr(0, indexOfArrow)
+        title = $.trim(title)
+      }
+      $('#techTweet').val(title + byAuthor + hashTags + cc)
+
+      let tabUrl = tab.url
+      const utmIndex = tabUrl.indexOf('?utm_source=feedburner')
+      if (utmIndex > -1) {
+        tabUrl = tabUrl.substr(0, utmIndex)
+      }
+      getAndAppendShortUrl(tabUrl, shortUrl => {
+        const techTweet = $('#techTweet')
+        const oldValue = techTweet.val()
+        techTweet.val(oldValue + ' ' + shortUrl + cc)
+        techTweet[0].focus()
+        techTweet[0].select()
+        techTweet.trigger('keyup')
+      })
+    })
+  })
+
+  function getAndAppendShortUrl(longUrl, callback) {
+    chrome.extension.sendRequest({ longUrl: longUrl }, response => {
+      callback(response.shortUrl)
+    })
+  }
+})
